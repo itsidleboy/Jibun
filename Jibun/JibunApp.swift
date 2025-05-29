2d845f9abc24491b7a9a71871bbffa8015cafb92
Idle Boy <tougher.steppe.0t@icloud.com>
2025-05-29 23:04:42 +0530

    Initial Commit

---
+++ Jibun/JibunApp.swift
@@ -0,0 +1,17 @@
+//
+//  JibunApp.swift
+//  Jibun
+//
+//  Created by Rahul on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct JibunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
